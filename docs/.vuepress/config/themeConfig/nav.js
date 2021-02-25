99f76185f16f0b612c0385b6575795da21dab65d
ch0769 <445750168@qq.com>
2021-02-25 15:09:12 +0800

    master
    Signed-off-by: ch0769 <445750168@qq.com>

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -2,56 +2,51 @@
 module.exports = [
   { text: '首页', link: '/' },
   {
-    text: '前端',
-    link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: '电脑',
+    link: '/pc/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: '前端文章',
+        text: '操作系统',
         items: [
-          { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-          { text: 'Vue', link: '/pages/5d463fbdb172d43b/' },
+		  
+          { text: 'windows', link: '/posts/14.html' },		 
+          { text: 'linux', link: '/posts/27.html' }
+		  
         ],
       },
       {
-        text: '学习笔记',
+        text: '硬件',
         items: [
-          { text: '《JavaScript教程》笔记', link: '/note/javascript/' },
-          { text: '《JavaScript高级程序设计》笔记', link: '/note/js/' },
-          { text: '《ES6 教程》笔记', link: '/note/es6/' },
-          { text: '《Vue》笔记', link: '/note/vue/' },
+          { text: '《CPU》', link: '/note/javascript/' },
+          { text: '《内存》', link: '/note/js/' },
+          { text: '《主板》', link: '/note/es6/' },
+          { text: '《硬盘》', link: '/note/vue/' },
           {
-            text: '《TypeScript 从零实现 axios》',
+            text: '《电源》',
             link: '/note/typescript-axios/',
-          },
-          {
-            text: '《Git》学习笔记',
-            link: '/note/git/',
-          },
-          {
-            text: '《TypeScript》笔记',
-            link: '/pages/51afd6/',
-          },
+                  },
         ],
       },
     ],
   },
   {
-    text: '页面',
-    link: '/ui/',
+    text: '手机',
+    link: '/phone/',
     items: [
-      { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-      { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+      
+	  { text: '手机软件', link: '/pages/18.html' },
+      { text: '手机系统', link: '/pages/18.html' },
     ],
   },
   {
-    text: '技术',
-    link: '/technology/',
+    text: '网络技术',
+    link: '/web/',
     items: [
-      { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-      { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-      { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-      { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+      { text: '玩转矿渣', link: '/posts/20.html' },
+      { text: 'GitHub相关', link: '/pages/4c778760be26d8b3/' },
+      { text: '网络技巧', link: '/posts/13.html' },
+      { text: '博客搭建', link: '/posts/2.html' },
     ],
   },
   {
